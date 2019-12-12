9ebc2d137411f3c0079d2312bcf859d7e358d837
jjuiddong <jjuiddong@hanmail.net>
2019-12-12 17:17:56 +0900

    update ProtocolGenerator Project

---
+++ Src/ProtocolGenerator/pch.h
@@ -0,0 +1,11 @@
+// precompiled header
+
+#pragma once
+
+
+#include "../../../Common/Common/common.h"
+#include "../../../Common/Network2/network2.h"
+
+
+#include <comutil.h>
+#include "GenerateCode.h"
